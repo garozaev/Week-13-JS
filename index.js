b4dd3ec69c8fe75f9a379a48b2a23d8afebe6268
garozaev <g.a.rozaev@gmail.com>
2023-11-01 17:52:09 +0300

    JS N1

---
+++ index.js
@@ -0,0 +1,50 @@
+let message = 'Привет';
+alert(message);
+
+let x = 5;
+let y = 5;
+let use = 'Geo';
+let age = 52;
+const surmane = 'Rozaev';
+let house;
+let name = 'Gorg';
+let message2 = `Привет, ${name}`;
+alert(message2);
+alert(house);
+let z = (x*y);
+alert(5 + 5);
+alert(x + y);
+alert(z);
+let yes = true;
+let no = false;
+
+let n = null;
+alert(n);
+
+let message_to = 'Какдела';
+message_to +=' '+ use;
+alert(message_to);
+
+let a = '5';
+let b = '2';
+
+alert(Number(a) + Number(b));
+alert(Boolean('0'));
+alert(Boolean (0));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
